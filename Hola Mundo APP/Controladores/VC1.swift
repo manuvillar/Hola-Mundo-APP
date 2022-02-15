9ca69d511f6d4139436e2808321a8deb0233cfa2
Manu <mjvillar@iesoretania.es>
2022-02-15 13:39:19 +0100

    Inicial

---
+++ Hola Mundo APP/Controladores/VC1.swift
@@ -0,0 +1,18 @@
+//
+//  VC1.swift
+//  Hola Mundo APP
+//
+//  Created by Manuel J. Villar on 15/2/22.
+//
+
+import UIKit
+
+class VC1: UIViewController{
+    
+    @IBAction func accion(_ sender: Any) {
+        
+    }
+    @IBAction func CampoText(_ sender: Any) {
+        
+    }
+}
